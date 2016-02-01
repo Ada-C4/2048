edddbab950fef51fd08bbf3711a5d4dae7794c35
Logan McDonald <loganmcdona11@gmail.com>
2016-02-01 15:28:34 -0800

    can move around without problems, condensed in a box

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -11,22 +11,30 @@ Game.prototype.moveTile = function(tile, direction) {
       console.log('up');
       console.log(tile);
       var row = $(".tile")[0].dataset.row;
-      $(".tile")[0].dataset.row = row[0] + (parseInt(row[1]) - 1);
+      if (row[1] - 1 >= 0) {
+        $(".tile")[0].dataset.row = row[0] + (parseInt(row[1]) - 1);
+      }
       break;
     case 40: //down
       console.log('down');
       var row = $(".tile")[0].dataset.row;
-      $(".tile")[0].dataset.row = row[0] + (parseInt(row[1]) + 1);
+      if (parseInt(row[1]) + 1 <= 3) {
+        $(".tile")[0].dataset.row = row[0] + (parseInt(row[1]) + 1);
+      }
       break;
     case 37: //left
       console.log('left');
       var column = $(".tile")[0].dataset.col;
-      $(".tile")[0].dataset.col = column[0] + (parseInt(column[1]) - 1);
+      if (column[1] - 1 >= 0) {
+        $(".tile")[0].dataset.col = column[0] + (parseInt(column[1]) - 1);
+      };
       break;
     case 39: //right
       console.log('right');
       var column = $(".tile")[0].dataset.col;
-      $(".tile")[0].dataset.col = column[0] + (parseInt(column[1]) + 1);
+      if (parseInt(column[1]) + 1 <= 3) {
+        $(".tile")[0].dataset.col = column[0] + (parseInt(column[1]) + 1);
+      };
       break;
   }
 };
