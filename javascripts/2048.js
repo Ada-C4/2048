addcb77fc7914d4f5c7078622c6e4de2b2318622
Claire <ClaireS29@gmail.com>
2016-02-01 16:55:08 -0800

    created moveTile method to make first tile move all directions

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,5 +1,10 @@
 var Game = function() {
   // Game logic and initialization here
+  this.board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+};
+
+Game.prototype.play = function() {
+
 };
 
 Game.prototype.moveTile = function(tile, direction) {
@@ -7,15 +12,23 @@ Game.prototype.moveTile = function(tile, direction) {
   switch(direction) {
     case 38: //up
       console.log('up');
+      var upRow = parseInt($(".tile").attr("data-row").slice(-1)) - 1;
+      tile[0].setAttribute("data-row", ("r" + upRow));
       break;
     case 40: //down
       console.log('down');
+      var downRow = parseInt($(".tile").attr("data-row").slice(-1)) + 1;
+      tile[0].setAttribute("data-row", ("r" + downRow));
       break;
     case 37: //left
       console.log('left');
+      var leftCol = parseInt($(".tile").attr("data-col").slice(-1)) - 1;
+      tile[0].setAttribute("data-col", ("c" + leftCol));
       break;
     case 39: //right
       console.log('right');
+      var rightCol = parseInt($(".tile").attr("data-col").slice(-1)) + 1;
+      tile[0].setAttribute("data-col", ("c" + rightCol));
       break;
   }
 };
@@ -29,7 +42,7 @@ $(document).ready(function() {
     var arrows = [37, 38, 39, 40];
     if (arrows.indexOf(event.which) > -1) {
       var tile = $('.tile');
-      
+
       game.moveTile(tile, event.which);
     }
   });
