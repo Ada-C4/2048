8113567840835aab0ff4b6315eff0804f3c00ef0
Ricky Hougland <houglande@gmail.com>
2016-02-02 15:41:04 -0800

    get down button working

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -32,58 +32,80 @@ Game.prototype.moveTile = function(tile, direction) {
       console.log(self.gameBoard);
       break;
     case 40: //down
-      // console.log('down');
-      // $('.tile').animate({ top: '+=135'}, 100);
-      // break;
+      console.log('down');
+
+      // iterate through each "row" (each array within gameboard array)
+      self.gameBoard.forEach(function(row, rowIndex){
+        // iterate through each "column" (each item within a row)
+        row.forEach(function(column, columnIndex){
+          var numSpaces = 0;
+          // don't look at tiles that don't have a a value to them, AND don't look left if column is farthest down already
+          if (column !== 0 && rowIndex < 3) {
+            // iterate through each item further up of current item
+            for (var i = rowIndex; i < 3; i++) {
+              // increment numSpaces to move up by 1 if the next num up is 0
+              console.log(self.gameBoard[i + 1][columnIndex]);
+              if (self.gameBoard[i + 1][columnIndex] === 0) {
+                numSpaces += 1;
+              }
+            }
+            self.gameBoard[rowIndex + numSpaces][columnIndex] = column;
+            row[columnIndex] = 0;
+          }
+        });
+      });
+      console.log(self.gameBoard);
+
+      break;
     case 37: //left
-      // console.log('left');
-      // // iterate through each "row" (each array within gameboard array)
-      // self.gameBoard.forEach(function(row, rowIndex){
-      //   // iterate through each "column" (each item within a row)
-      //   row.forEach(function(column, columnIndex){
-      //     // don't look at tiles that don't have a a value to them, AND don't look left if column is farthest left
-      //     if (column !== 0 && columnIndex !== 0) {
-      //       // iterate through each item further left of current item
-      //       for (var i = columnIndex; i >= 0; i--) {
-      //         // increment numSpaces to move left by 1 if the next left over is 0
-      //         if (self.gameBoard[rowIndex][i-1] === 0) {
-      //           row[columnIndex - 1] = column;
-      //           row[columnIndex] = 0;
-      //         }
-      //       }
-      //     }
-      //   });
-      // });
-      // console.log(self.gameBoard);
-      //
-      // break;
+      console.log('left');
+      // iterate through each "row" (each array within gameboard array)
+      self.gameBoard.forEach(function(row, rowIndex){
+        // iterate through each "column" (each item within a row)
+        row.forEach(function(column, columnIndex){
+          // don't look at tiles that don't have a a value to them, AND don't look left if column is farthest left
+          if (column !== 0 && columnIndex !== 0) {
+            // iterate through each item further left of current item
+            for (var i = columnIndex; i >= 0; i--) {
+              // increment numSpaces to move left by 1 if the next left over is 0
+              if (self.gameBoard[rowIndex][i-1] === 0) {
+                row[columnIndex - 1] = column;
+                row[columnIndex] = 0;
+              }
+            }
+          }
+        });
+      });
+      console.log(self.gameBoard);
+
+      break;
     case 39: //right
-      // console.log('right');
-      // // iterate through each "row" (each array within gameboard array)
-      // // self.gameBoard.forEach(function(row, rowIndex){
-      // for (var k = 0; k < self.gameBoard.length; k++) {
-      //   // iterate through each "column" (each item within a row)
-      //   var row = self.gameBoard[k], rowIndex = k;
-      //
-      //   for (var j = 0; j < row.length; j++) {
-      //     var column = row[j], columnIndex = j;
-      //     // don't look at tiles that don't have a a value to them, AND don't look right if column is farthest right
-      //
-      //     if (column !== 0) {
-      //       if (columnIndex <= 3) {
-      //
-      //         // increment numSpaces to move right by 1 if the next right over is 0
-      //         if (self.gameBoard[rowIndex][columnIndex + 1] === 0) {
-      //           // select current tile and move it appropriate num spaces right
-      //           row[columnIndex + 1] = column;
-      //           row[columnIndex] = 0;
-      //         }
-      //       }
-      //     }
-      //   }
-      // }
-      // console.log(self.gameBoard);
-      // break;
+      console.log('right');
+      // iterate through each "row" (each array within gameboard array)
+      // self.gameBoard.forEach(function(row, rowIndex){
+      for (var k = 0; k < self.gameBoard.length; k++) {
+        // iterate through each "column" (each item within a row)
+        var row = self.gameBoard[k], rowIndex = k;
+
+        for (var j = 0; j < row.length; j++) {
+          var column = row[j], columnIndex = j;
+          // don't look at tiles that don't have a a value to them, AND don't look right if column is farthest right
+
+          if (column !== 0) {
+            if (columnIndex <= 3) {
+
+              // increment numSpaces to move right by 1 if the next right over is 0
+              if (self.gameBoard[rowIndex][columnIndex + 1] === 0) {
+                // select current tile and move it appropriate num spaces right
+                row[columnIndex + 1] = column;
+                row[columnIndex] = 0;
+              }
+            }
+          }
+        }
+      }
+      console.log(self.gameBoard);
+      break;
   }
 };
 
