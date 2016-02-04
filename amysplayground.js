40207d21eb24518f5e8676538e6d64a920473cdd
Amy Hunter <amyrhunter@gmail.com>
2016-02-04 12:20:28 -0800

    Add more logic to pull a two or a 4 and assign it to an empty value in the game board. Not working yet.

--- amysplayground.js
+++ amysplayground.js
@@ -7,24 +7,35 @@
 // // 2. Select a random start value
 // var result = ['January', 'February', 'March'][Math.floor(Math.random() * 3)]
 // function getTwoOrFour(){
-//   var result = [2, 4][Math.floor(Math.random() * 3)];
+  // var result = [2, 4][Math.floor(Math.random() * 3)];
 //   return result;
 // }
+
+Game.prototype.getFreeSpaces = function(){
 // getTwoOrFour()
-var gameBoard = [[0, 2, 0, 0], [0, 2, 0, 0], [0, 4, 4, 0], [0, 0, 4, 0]];
-var freeSpaces = [];
+  var result = [2, 4][Math.floor(Math.random() * result.length)];
+  var gameBoard = [[0, 2, 0, 0], [0, 2, 0, 0], [0, 4, 4, 0], [0, 0, 4, 0]];
+  var freeSpaces = [];
 // 3. Iterate through the rows and the columns in the gameboard
-for(var i = 0; i < 4; i++){
-  for(var j = 0; j < 4; j++){
+  for(var i = 0; i < 4; i++){
+    for(var j = 0; j < 4; j++){
     // and find all that are empty
-    if (gameBoard[i][j] === 0){
+      if (gameBoard[i][j] === 0){
       // keep a list of the indices in a new array saved as a variable
-      freeSpaces.push([i, j]);
+        freeSpaces.push([i, j]);
+      }
     }
+  return freeSpaces;
   }
-}
-// getback a list of the indices of all free spaces on the board
+};
+
+var openSpots = this.getFreeSpaces();
+var selectedSpot = openSpots[Math.floor(Math.random() * avail.length)];
+this.gameBoard[selectedSpot[0]][selectedSpot[1]] = result;
 console.log(freeSpaces);
+console.log(result);
+
+
 
 //
 // this.gameBoard[1]
